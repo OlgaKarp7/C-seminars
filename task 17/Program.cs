968b9b5022bcbf42d72e608e6d79fb1dcea48c91
OlgaKarpova <olialesha2006@mail.ru>
2022-08-21 10:11:47 +0300

    Решили задачу 17

--- task 17/Program.cs
+++ task 17/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать задачу, которая принимает на вход координаты точки (X, Y), причем x!= 0 и y!= 0 и выдает номер четверти плоскост, в которой находится эта точка.
+
+Console.Write("Введите x: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите y: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.Write("1");
+}
+if (x < 0 && y > 0)
+{
+    Console.Write("2");
+}
+if (x < 0 && y < 0)
+{
+    Console.Write("3");
+}
+if (x > 0 && y < 0)
+{
+    Console.Write("4");
+}
