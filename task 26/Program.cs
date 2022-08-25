4d67d3b97128f01221152d1aa042fcf93381ed34
OlgaKarpova <olialesha2006@mail.ru>
2022-08-25 12:51:26 +0300

    Решили задачу 26

---
+++ task 26/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Написать программу, которая принимает на вход число и выдает кол-во цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+Console.Write(" Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int DigitCount(int a)
+{
+    int count= 0;
+    while(a > 0)
+    {
+        count++;
+        a /= 10;  //a = a / 10; 
+    }
+    return count;
+}
+
+Console.Write(DigitCount(num));
