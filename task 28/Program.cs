55459eb685848c6778421c95b0153236cb1fea2d
OlgaKarpova <olialesha2006@mail.ru>
2022-09-05 11:25:08 +0300

    Решили задачу 28

---
+++ task 28/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Написать программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+4 -> 24
+5 -> 120*/
+
+Console.Write(" Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int MultiDigital(int N)
+{
+    int a = 1;
+    
+    for(int i = 2; i <= N; i++)
+    {
+         a = a * i;
+    }
+    return a;
+}
+
+Console.WriteLine(MultiDigital(num));
