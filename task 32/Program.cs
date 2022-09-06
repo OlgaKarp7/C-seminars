6e86b6c880a85d4e1601633b730a3c430b75321e
OlgaKarpova <olialesha2006@mail.ru>
2022-09-06 22:44:25 +0300

    Решили задачу 32

---
+++ task 32/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Написать программу замены эл.массива: положительные заменить на соответствующие отрицательные и наоборот. */
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0;i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+
+    }
+    return res;
+}
+
+int[] rev(int[] mas)
+{
+    for( int i = 0; i < mas.Length; i++)
+    {
+        mas[i] *= -1;
+    }
+    return mas;
+}
+
+
+int[] array = GetArray(5, -10, 11);
+Console.WriteLine(String.Join(" ", array));
+
+Console.WriteLine(String.Join(" ", rev(array)));
