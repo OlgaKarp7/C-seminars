32c9a0cbf7bb208d45532fb7b29da0ea82d478e8
OlgaKarpova <olialesha2006@mail.ru>
2022-09-06 23:36:15 +0300

    Решили задачи 32, 3

---
+++ task 35/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задать одномерный массив из 123 случайных чмсел. Найти кол-во эл.массива, значения которых лежат в отрезке [10, 99]
+*/
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0;i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+
+    }
+    return res;
+}
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(" ", array));
