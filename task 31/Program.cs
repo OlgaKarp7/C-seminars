823821a38649063cdc4d3779b42a819784dfbafd
OlgaKarpova <olialesha2006@mail.ru>
2022-09-06 22:12:16 +0300

    Решили задачу 31

---
+++ task 31/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задать массив из 12 эл.со случайными числами [-9,9]. Найти сумму отрицательных и положительных эл. массива.*/
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0;i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+
+    }
+    return res;
+}
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(" ", array));
+
+int positiveSum = 0;
+int negativeSum = 0;
+
+foreach (int el in array)
+{
+    positiveSum += el > 0 ? el : 0; //if (el > 0) { positiveSum += el;} else { positiveSum += 0;}
+    negativeSum += el < 0? el : 0;
+}
+
+Console.WriteLine($"PositiveSum = {positiveSum}, NegativeSum = {negativeSum}");
