e2d3223e91a6dfa0926942009328c13494a9bcc5
OlgaKarpova <olialesha2006@mail.ru>
2022-09-10 13:09:05 +0300

    Решили задачу 39

--- task 37/Program.cs
+++ task 37/Program.cs
@@ -1,3 +1,33 @@
 ﻿/*Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т. д. Результат запишите в новом массиве.*/
 
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
 
+    for(int i = 0; i < res.Length; i++)
+    {
+      res[i] = new Random().Next(1, 100);
+    }
+
+    return res;
+}
+
+int[] ResultArray(int[] array)
+{
+    int size = array.Length / 2;
+    if (array.Length % 2 == 1) size++;
+    int[] result = new int[size];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        
+        result[i] = array[i] * array[array.Length - 1 - i]; //array.Length - 1 означает последний элемент, -i значит, что двигаемся от последнего эл. в сторону первого. 5-1=4-i
+    }
+    if (array.Length % 2 == 1) result[size-1] = array[array.Length / 2];
+    return result;
+}
+
+
+int[] array = GetArray(5);
+int[] GroupArr = ResultArray(array);
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine(String.Join(" ", GroupArr));
