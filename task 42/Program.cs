842891c66cf81cb50c2187f634447215fc227099
OlgaKarpova <olialesha2006@mail.ru>
2022-09-13 23:21:49 +0300

    Решили задачу 42

---
+++ task 42/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Написать прог, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10*/
+
+Console.Write(" Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int GetBinary(int num)
+{
+    string result = ""; /* или String.Empty - пустая строчка*/
+    while(num > 0)
+    {
+        result = result + Convert.ToString(num % 2);
+        num /= 2; // num=num/2
+        
+    }
+    result. ToCharArray().Reverse(); //строку дробим на массив букв, а потом переворачиваем
+    return int.Parse(result);
+}
+Console.WriteLine($"{GetBinary(number)}");
