82c615ce06033fdd2962f684e6ca6f19079d8adb
OlgaKarpova <olialesha2006@mail.ru>
2022-09-13 23:45:53 +0300

    Решили задачу 44

---
+++ task 44/Program.cs
@@ -0,0 +1,18 @@
+﻿/* не используя рекурсию, вывести первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1*/
+
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+void FibNum(int n)
+{
+    int[] fibarr = new int[n];
+    fibarr[0] = 0;
+    fibarr[1] = 1;
+    for(int i = 2; i < n; i++)
+    {
+        fibarr[i] = fibarr[i - 1] + fibarr[i - 2];
+    }
+    var str = string.Join(" ", fibarr);
+    Console.WriteLine($"Первые {n} чисел Фибоначчи: {str}");
+}
+
+FibNum(n);
