9c92f0700c44e190605112b18b7a638d946355a1
OlgaKarpova <olialesha2006@mail.ru>
2022-09-15 17:36:59 +0300

    Решили задачу 49

---
+++ task 48/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задайте двумерный массив размером m x n, каждый элемент в массиве находится по формуле: A(mn)=m+n . Выведите полученный массив на экран.*/
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }  
+            return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+           Console.Write($"{array[i, j]} ");
+        Console.WriteLine();     
+    }
+}  
+
+Console.Write("m = ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] myArray = GetArray(m, n, 0, 100);
+PrintArray(myArray);
---
+++ task 49/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задать двумерный массив. Найти элем, у которых оба индекса четные, и заменить эти элементы на их квадраты.*/
+
+Console.Write("rows = ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("colums = ");
+int colums = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int rows, int colums)
+{
+    int[,] result = new int[rows, colums];
+
+    for (int i = 0; i < result.GetLength(0); i++)
+        for (int j = 0; j < result.GetLength(1); j++)
+            result[i, j] = new Random().Next(1, 10);
+
+            return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+           Console.Write($"{array[i, j]} ");
+        Console.WriteLine();     
+    }
+}  
+
+int[,] CorrectArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] *= array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+int[,] myArray = GetArray(rows, colums);
+PrintArray(myArray);
+Console.WriteLine();
+myArray = CorrectArray(myArray);
+PrintArray(myArray);
